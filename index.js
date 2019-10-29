07ffa2eaa8169c2dde303cca3d276b3c18d56918
tlahique <55733927+tlahique@users.noreply.github.com>
2019-10-29 02:47:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,23 +7,21 @@ $(document).ready(function () {
 
 var map = L.map('map');
 
-//'http://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png'
-
+//Add the main map, OpenStreeMap Vector Beta
 L.tileLayer('http://esri.maps.arcgis.com/apps/View/index.html?appid=d6b18a2e774c4959ba855f6ac90952a2', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
 	maxZoom: 19
 }).addTo(map);
 
-map.setView(new L.LatLng(25.72650, -100.31180), 16);
-
-
+//Add shadows to the map
 var OpenMapSurfer_Hillshade = L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/asterh/webmercator/{z}/{x}/{y}.png', {
-	maxZoom: 18,
+	maxZoom: 19,
 	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> | Map data  <a href="https://lpdaac.usgs.gov/products/aster_policies">ASTER GDEM</a>, <a href="http://srtm.csi.cgiar.org/">SRTM</a>'
-});
+}).addTo(map);
 
-
+//Set the map to point at UANL
+map.setView(new L.LatLng(25.72650, -100.31180), 17);
 
 // 'control' previously called 'routingControl'
 var control = L.Routing.control({
